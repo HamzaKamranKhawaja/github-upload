dbeb669e2925652e0c4b5aa6adcba20ea3248d0e
Hamza Kamran Khawaja <hamza.khawaja@berkeley.edu>
2020-03-02 17:55:47 -0800

    Completed lab6

--- lab6/enigma/PermutationTest.java
+++ lab6/enigma/PermutationTest.java
@@ -82,5 +82,122 @@ public abstract class PermutationTest {
         checkPerm("identity", UPPER_STRING, UPPER_STRING, perm, alpha);
     }
 
-    // FIXME: Add tests here that pass on a correct Permutation and fail on buggy Permutations.
+    @Test
+    public void testInvertChar() {
+        Permutation p = getNewPermutation("(BACD)", getNewAlphabet("ABCD"));
+        assertEquals("Expected B",'B', p.invert('A'));
+        assertEquals("Expected D",'D', p.invert('B'));
+        assertEquals("Expected A",'A', p.invert('C'));
+    }
+
+    @Test
+    public void testSize() {
+        Permutation p = getNewPermutation("(BACD)", getNewAlphabet("ABCD"));
+        assertEquals("Should be size 4",4, p.size());
+
+        Permutation b = getNewPermutation("(S)", getNewAlphabet("S"));
+        assertEquals("Should be size 1",1, b.size());
+
+    }
+
+    @Test
+    public void testpermute() {
+        Permutation p = getNewPermutation("(BACD)", getNewAlphabet("ABCDE"));
+        assertEquals("Permute should point 2",2, p.permute(0));
+        assertEquals("Permute should point 0",0, p.permute(1));
+        assertEquals("Permute should point 3",3, p.permute(2));
+        assertEquals("Permute should point 1",1, p.permute(3));
+        assertEquals("Permute should point 4",4, p.permute(4));
+
+        assertEquals("Permute should point 2",'C', p.permute('A'));
+        assertEquals("Permute should point 0",'A', p.permute('B'));
+        assertEquals("Permute should point 3",'D', p.permute('C'));
+        assertEquals("Permute should point 1",'B', p.permute('D'));
+        assertEquals("Permute should point 4",'E', p.permute('E'));
+
+        assertEquals("expected size 5",5, p.size());
+        assertFalse(p.derangement());
+
+        Permutation self = getNewPermutation("(S)", getNewAlphabet("S"));
+        assertEquals("Permute self should point correctly 0->0",0, self.permute(0));
+
+        Permutation multi = getNewPermutation("   (S)     (T) (E)   ", getNewAlphabet("STE"));
+        assertEquals("Permute should point S",'S', multi.permute('S'));
+        assertEquals("Permute should point T",'T', multi.permute('T'));
+        assertEquals("Permute should point E",'E', multi.permute('E'));
+    }
+
+    @Test
+    public void testinvert() {
+        Permutation p = getNewPermutation("(BACD)", getNewAlphabet("ABCDE"));
+        assertEquals("Invert incorrect 1",1, p.invert(0));
+        assertEquals("Invert incorrect 2",2, p.invert(3));
+        assertEquals("Invert incorrect 1",0, p.invert(2));
+        assertEquals("Invert incorrect 0",3, p.invert(1));
+        assertEquals("Invert incorrect 4",4, p.invert(4));
+
+        assertEquals("Invert incorrect 1",'B', p.invert('A'));
+        assertEquals("Invert incorrect 2",'C', p.invert('D'));
+        assertEquals("Invert incorrect 1",'A', p.invert('C'));
+        assertEquals("Invert incorrect 0",'D', p.invert('B'));
+        assertEquals("Invert incorrect 4",'E', p.invert('E'));
+
+        assertEquals("expected size 5",5, p.size());
+        assertFalse(p.derangement());
+
+        Permutation self = getNewPermutation("(S)", getNewAlphabet("S"));
+        assertEquals("Single invert should point to itself 0<-0", 0, self.invert(0));
+
+        Permutation multi = getNewPermutation("   (S)     (T) (E)   ", getNewAlphabet("STE"));
+        assertEquals("invert should point S",'S', multi.invert('S'));
+        assertEquals("invert should point T",'T', multi.invert('T'));
+        assertEquals("invert should point E",'E', multi.invert('E'));
+
+    }
+
+    @Test(expected = EnigmaException.class)
+    public void testNotInAlphabet() {
+        Permutation p = getNewPermutation("(BACD)", getNewAlphabet("ABCD"));
+        p.invert('F');
+    }
+
+    @Test(expected = EnigmaException.class)
+    public void testNotInAlphabetTwo() {
+        Permutation p = getNewPermutation("(BACD)", getNewAlphabet("ABCD"));
+        p.permute('F');
+    }
+
+
+    @Test
+    public void testAlphabet() {
+        Alphabet test = getNewAlphabet("ABCD");
+        Permutation p = getNewPermutation("(BACD)", test);
+
+        assertEquals(test, p.alphabet());
+
+        Alphabet testTwo = getNewAlphabet("ABCDE");
+        Permutation b = getNewPermutation("(BACDE)", testTwo);
+
+        assertNotEquals(test, b.alphabet());
+    }
+
+    @Test
+    public void derangementTest() {
+        Permutation p = getNewPermutation("(BACD)", getNewAlphabet("ABCD"));
+        assertTrue("Should be true since cycles is (BACD)", p.derangement());
+
+        Permutation b = getNewPermutation("(B)", getNewAlphabet("B"));
+        assertFalse("Should be false since cycles is (B)",b.derangement());
+
+        Permutation bTwo = getNewPermutation("(D)      (ABC)", getNewAlphabet("ABCD"));
+        assertFalse("Should be false since cycles is (D) (ABC)",bTwo.derangement());
+
+        Permutation c = getNewPermutation("", getNewAlphabet("BAC"));
+        assertFalse("All should be false",c.derangement());
+
+        Permutation d = getNewPermutation("(A) (B)    (C)", getNewAlphabet("BAC"));
+        assertFalse(d.derangement());
+    }
+
+
 }
