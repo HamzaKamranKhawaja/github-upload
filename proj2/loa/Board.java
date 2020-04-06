ecce4429e416a365dc18ed3f8de425a8c2a34113
Hamza Kamran Khawaja <hamza.khawaja@berkeley.edu>
2020-04-06 12:47:11 -0700

    removed print statements

--- proj2/loa/Board.java
+++ proj2/loa/Board.java
@@ -128,7 +128,6 @@ class Board {
         if (firstP != endP && endP != EMP) {
             move = move.captureMove();
         }
-        System.out.println(this.toString());
         _moves.add(move);
         set (endS, firstP);
         set (firstS, EMP);
