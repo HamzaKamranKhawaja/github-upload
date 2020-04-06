baa8582b0d6a9ae220e3b36e6250fdc821842637
Hamza Kamran Khawaja <hamza.khawaja@berkeley.edu>
2020-04-05 21:07:41 -0700

    added new lines to stds

--- proj2/loa/MachinePlayer.java
+++ proj2/loa/MachinePlayer.java
@@ -123,7 +123,7 @@ class MachinePlayer extends Player {
 
     /** Return a search depth for the current position. */
     private int chooseDepth() {
-        return 3;  // FIXME
+        return 5;  // FIXME
     }
 
     // FIXME: Other methods, variables here.
