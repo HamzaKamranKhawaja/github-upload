7a5b97ca5b0d374fa24d97f6e83f4a7f66dc4d98
Hamza Kamran Khawaja <hamza.khawaja@berkeley.edu>
2020-04-16 21:39:02 -0700

    gitbug submission

--- hw8/RedBlackTree.java
+++ hw8/RedBlackTree.java
@@ -171,7 +171,7 @@ public class RedBlackTree<T extends Comparable<T>> {
             // YOUR CODE HERE
 
                 node.right = insert(node.right, item);
-                node.right.isBlack = true;
+                node.right.isBlack = false;
 
         }
 
